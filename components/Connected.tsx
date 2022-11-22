fc5881955aae8c15eebbee0c6e23d2dbe49ac6db
Sandeep Krishna <sandeep@MacBook-Pro-2.local>
2022-11-22 23:46:14 +0530

    Added Candy Machine and Mint Buildoor implementation

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,40 +1,104 @@
-
-import { FC } from "react"
 import {
   Button,
   Container,
   Heading,
-  HStack,
-  Text,
   VStack,
+  Text,
+  HStack,
   Image,
 } from "@chakra-ui/react"
-import { ArrowForwardIcon } from "@chakra-ui/icons"
-
+import {
+  FC,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react"
+import { PublicKey } from "@solana/web3.js"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import {
+  Metaplex,
+  walletAdapterIdentity,
+  CandyMachine,
+  CandyMachineV2
+} from "@metaplex-foundation/js"
+import { useRouter } from "next/router"
 
 const Connected: FC = () => {
-    return (
-      <VStack spacing={10}>
-        <Container>
-          <VStack spacing={8}>
-            <Heading
-              color="white"
-              as="h1"
-              size="2xl"
-              noOfLines={1}
-              textAlign="center"
-            >
-              Welcome Buildoor.
-            </Heading>
-  
-            <Text color="bodyText" fontSize="xl" textAlign="center">
-              Each buildoor is randomly generated and can be staked to receive
-              <Text as="b"> $BLD</Text> Use your <Text as="b"> $BLD</Text> to
-              upgrade your buildoor and receive perks within the community!
-            </Text>
-          </VStack>
-        </Container> 
-    <HStack spacing={4}>
+  const { connection } = useConnection()
+  const walletAdapter = useWallet()
+  const [candyMachine, setCandyMachine] = useState<CandyMachineV2>()
+  const [isMinting, setIsMinting] = useState(false)
+
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
+  }, [connection, walletAdapter])
+
+  useEffect(() => {
+    if (!metaplex) return
+
+    metaplex
+      .candyMachinesV2()
+      .findByAddress({
+        address: new PublicKey("8hjFHgmcTY4DKuaZHziKi3cDfV7cQVbPQJAHWf4uGyLR"),
+      })
+      .then((candyMachine) => {
+        setCandyMachine(candyMachine)
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }, [metaplex])
+
+  const router = useRouter()
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+      if (event.defaultPrevented) return
+
+      if (!walletAdapter.connected || !candyMachine) {
+        return
+      }
+
+      try {
+        setIsMinting(true)
+        const nft = await metaplex.candyMachinesV2().mint({
+          candyMachine,
+    });
+        router.push(`/newMint?mint=${nft.nft.address.toBase58()}`)
+      } catch (error) {
+        alert(error)
+      } finally {
+        setIsMinting(false)
+      }
+    },
+    [walletAdapter.connected, candyMachine, metaplex, router]
+  )
+
+  return (
+    <VStack spacing={10}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Welcome Buildoor.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each buildoor is randomly generated and can be staked to receive
+            <Text as="b"> $BLD</Text>. Use your <Text as="b"> $BLD</Text> to
+            upgrade your buildoor and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={4}>
         <Image src="images/avatar1.png" alt="" />
         <Image src="images/avatar2.png" alt="" />
         <Image src="images/avatar3.png" alt="" />
@@ -42,11 +106,14 @@ const Connected: FC = () => {
         <Image src="images/avatar5.png" alt="" />
       </HStack>
 
-      <Button bgColor="blue" color="white" maxW="380px">
-        <HStack>
-          <Text>mint buildoor</Text>
-          <ArrowForwardIcon />
-        </HStack>
+      <Button
+        bgColor="blue"
+        color="white"
+        maxW="380px"
+        onClick={handleClick}
+        isLoading={isMinting}
+      >
+        <Text>mint buildoor</Text>
       </Button>
     </VStack>
   )
